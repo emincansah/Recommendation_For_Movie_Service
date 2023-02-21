66e1ffe769b9ca8d8b4bce85447851a4b77df202
emincansah <sahiinemincan@gmail.com>
2023-02-21 17:50:54 +0300

    proje ayıklama

--- Recommendation_For_Movie_Service_/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Recommendation_For_Movie_Service_.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
